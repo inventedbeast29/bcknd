d37cdff6879022de57b9daaa63b117df07930142
Abhinav Rai <88543056+inventedbeast29@users.noreply.github.com>
2025-03-01 11:05:16 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -62,7 +62,7 @@ app.post("/",(req,res)=>{
 
 })
 const PORT=process.env.PORT||7000
-app.listen(PORT,"0.0.0.0"(err)=>{
+app.listen(PORT,"0.0.0.0",(err)=>{
    if(err) console.log("Server crash")
     else{
 console.log("Server started")}
